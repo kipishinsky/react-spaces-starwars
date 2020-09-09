1fb99500878ec340ea85a157d0e6ca56fbbffcf4
rbkmoneyukk@bk.ru <rbkmoneyukk@bk.ru>
2020-09-09 16:27:32 +0400

    add logic error

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react'
 import SwapiService from '../../api/swapi-api'
 import './random-planet.css'
 import Spinner from '../spinner/spinner'
+import ErrorIndicator from '../error-indication'
 
 
 export default class RandomPlanet extends Component {
@@ -11,7 +12,8 @@ export default class RandomPlanet extends Component {
 
 	state = {
 		planet: {},
-		loading: true
+		loading: true,
+		error: false
 	}
 
 	constructor() {
@@ -25,22 +27,34 @@ export default class RandomPlanet extends Component {
 	}
 
 
+	onError = (err) => {
+		this.setState({
+			error: true,
+			loading: false
+		})
+	}
+
 	updatePlanet() {
-		const id = Math.floor(Math.random() * 25 + 2)
+		const id = 12 //Math.floor(Math.random() * 25 + 2)
 		this.swapiService
 			.getPlanet(id)
 			.then(this.onPlanetLoaded)
+			.catch(this.onError)
 	}
 
 	render() {
 
-		const {planet, loading} = this.state
+		const {planet, loading, error} = this.state
 
+		const hasData = !(loading || error)
+
+		const errorMessage = error ? <ErrorIndicator /> : null
 		const spinner = loading ? <Spinner /> : null
-		const content = !loading ? <PlanetView planet={planet}/> : null
+		const content = hasData ? <PlanetView planet={planet}/> : null
 
 		return (
 			<div className="random-planet jumbotron rounded">
+				{errorMessage}
 				{spinner}
 				{content}
 			</div>
